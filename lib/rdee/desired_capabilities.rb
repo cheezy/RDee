4b81c7d4b977f98933a5c8ebe6575c05180e4828
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2017-07-18 07:44:27 -0500

    Cleanup of capabilities

--- lib/rdee/desired_capabilities.rb
+++ lib/rdee/desired_capabilities.rb
@@ -5,34 +5,34 @@ module RDee
     include MobileDevices
     include MobileCapabilities
 
-    def capabilities(platform, version, host, existing_capabilities)
-      existing_capabilities = {} unless existing_capabilities
-      return mobile_capabilities(platform, version, existing_capabilities) if mobile?(platform)
-      browser_capabilities(platform, version, host, existing_capabilities)
+    def capabilities(platform, version, host, existing_caps)
+      existing_caps = {} unless existing_caps
+      return mobile_capabilities(platform, version, existing_caps) if mobile?(platform)
+      browser_capabilities(platform, version, host, existing_caps)
     end
 
-    def browser_capabilities(platform, version, host, existing_capabilities)
+    private
+
+    def browser_capabilities(platform, version, host, existing_caps)
       capabilities = Selenium::WebDriver::Remote::Capabilities.send platform
       capabilities.version = version unless version.nil?
       capabilities.platform = host unless host.nil?
-      existing_capabilities.each do |key, value|
+      existing_caps.each do |key, value|
         capabilities[key] = value
       end
       capabilities
     end
 
-    def mobile_capabilities(platform, version, existing_capabilities)
-      existing_capabilities.merge!(additional_capabilities_for(platform))
-      capabilities = Selenium::WebDriver::Remote::Capabilities
-                         .new({
-                                  platformVersion: version,
-                                  platformName: mobile_platform[platform],
-                                  browserName: mobile_browsers[platform],
-                                  platform: :mac,
-                                  javascript_enabled: true
-                              }.merge(existing_capabilities))
-
+    def mobile_capabilities(platform, version, existing_caps)
+      existing_caps.merge!(additional_capabilities_for(platform))
+      Selenium::WebDriver::Remote::Capabilities.new(
+          {
+              platformVersion: version,
+              platformName: mobile_platform[platform],
+              browserName: mobile_browsers[platform],
+              platform: :mac,
+              javascript_enabled: true
+          }.merge(existing_caps))
     end
-
   end
 end
